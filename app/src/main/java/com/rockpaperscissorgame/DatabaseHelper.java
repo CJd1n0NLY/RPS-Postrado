07d6d222e630f76f940f93b34da8ca1409344e75
CJd1n0NLY <postradocharles.bsit@gmail.com>
2024-04-17 23:54:41 +0800

    RPS-Postrado

---
+++ app/src/main/java/com/rockpaperscissorgame/DatabaseHelper.java
@@ -0,0 +1,69 @@
+package com.rockpaperscissorgame;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "rpsDatabase.db";
+    private static final String TABLE_NAME = "rpsHistory";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_MATCHRESULT = "matchResult";
+
+    private static final String COLUMN_PLAYER = "playerChoice";
+    private static final String COLUMN_COMPUTER = "computerChoice";
+    private static final int DATABASE_VERSION = 1;
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    db.execSQL("CREATE TABLE " + TABLE_NAME + "(" + COLUMN_ID +  " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + COLUMN_PLAYER + " TEXT, " + COLUMN_COMPUTER + " TEXT, " + COLUMN_MATCHRESULT + " TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+//        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME2);
+
+        onCreate(db);
+
+    }
+
+    public boolean addToHistory(String playerChoice, String computerChoice, String matchResult){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_PLAYER, playerChoice);
+        values.put(COLUMN_COMPUTER, computerChoice);
+        values.put(COLUMN_MATCHRESULT, matchResult);
+
+       long result = db.insert(TABLE_NAME, null,values);
+
+       if(result == 1){
+           return false;
+       } else {
+           return true;
+       }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query,null);
+        }
+        return cursor;
+    }
+
+}
---
+++ app/src/main/java/com/rockpaperscissorgame/MainActivity.java
@@ -0,0 +1,216 @@
+package com.rockpaperscissorgame;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private static int userIndex;
+    private DatabaseHelper dbHelper;
+   private static String selectedHandSign;
+    private static final String[] handSigns = {"Rock", "Paper", "Scissors"};
+    private int wins = 0;
+    private int losses = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ImageView rockImage = findViewById(R.id.rockImage);
+        ImageView paperImage = findViewById(R.id.paperImage);
+        ImageView scissorsImage = findViewById(R.id.scissorsImage);
+        Button playButton = findViewById(R.id.playButton);
+        Button historyButton = findViewById(R.id.historyButton);
+        TextView playerSelects = findViewById(R.id.playerSelects);
+
+
+        dbHelper = new DatabaseHelper(this);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        rockImage.setOnClickListener(v -> {
+             userIndex = 0;
+            selectedHandSign = "Rock";
+            playerSelects.setText(selectedHandSign);
+
+        });
+
+        paperImage.setOnClickListener(v -> {
+             userIndex = 1;
+            selectedHandSign = "Paper";
+            playerSelects.setText(selectedHandSign);
+        });
+
+        scissorsImage.setOnClickListener(v -> {
+             userIndex = 2;
+            selectedHandSign = "Scissors";
+            playerSelects.setText(selectedHandSign);
+        });
+
+        playButton.setOnClickListener(v -> {
+            playGame();
+        });
+
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, History.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void displayToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).cancel();
+            }
+        }, 1000);
+    }
+
+    private void playGame() {
+        TextView botSelects = findViewById(R.id.botSelects);
+        TextView whoWins = findViewById(R.id.lblWhoWins);
+        TextView userVsComputer = findViewById(R.id.userVsComputer);
+
+        String result;
+
+        if (selectedHandSign == null) {
+            displayToast("Please select a hand sign first");
+            return;
+        }
+
+        Random random = new Random();
+        int computerIndex = random.nextInt(handSigns.length);
+        String computerHandSign = handSigns[computerIndex];
+
+        String userHandSign = handSigns[userIndex];
+
+        int userImage = getImage(userHandSign);
+        int computerImage = getImage(computerHandSign);
+
+        ImageView dispHands = findViewById(R.id.dispHands);
+        ImageView userHands = findViewById(R.id.userHands);
+        ImageView computerHands = findViewById(R.id.computerHands);
+
+        dispHands.setVisibility(View.INVISIBLE);
+        userHands.setImageResource(userImage);
+        computerHands.setImageResource(computerImage);
+
+        if (selectedHandSign.equals(computerHandSign)) {
+            result = "It's a tie!";
+            whoWins.setText(result);
+        } else if ((selectedHandSign.equals("Rock") && computerHandSign.equals("Scissors")) ||
+                (selectedHandSign.equals("Paper") && computerHandSign.equals("Rock")) ||
+                (selectedHandSign.equals("Scissors") && computerHandSign.equals("Paper"))) {
+            result = "Player Wins!";
+            whoWins.setText(result);
+            wins++;
+            saveWinsAndLosses(wins, losses);
+        } else {
+            result = "Computer Wins!";
+            whoWins.setText(result);
+            losses++;
+            saveWinsAndLosses(wins, losses);
+        }
+
+        botSelects.setText(computerHandSign);
+
+        TextView lblWin = findViewById(R.id.lblWin);
+        lblWin.setText(String.valueOf(wins));
+
+        TextView lblLose = findViewById(R.id.lblLose);
+        lblLose.setText(String.valueOf(losses));
+
+        String userVbot = "Player : " + selectedHandSign + " VS " + "Bot : " + computerHandSign;
+        userVsComputer.setText(userVbot);
+
+        final String playerChoice = selectedHandSign;
+        final String computerChoice = computerHandSign;
+        final String whoWin = result.trim();
+
+
+        try{
+            dbHelper.addToHistory(playerChoice, computerChoice, whoWin);
+        } catch (Exception e){
+            Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        selectedHandSign = null;
+
+    }
+
+    private int getImage(String handSign) {
+        int image;
+        switch (handSign) {
+            case "Rock":
+                image = R.drawable.rock1;
+                break;
+            case "Paper":
+                image = R.drawable.paper1;
+                break;
+            case "Scissors":
+                image = R.drawable.scissors1;
+                break;
+            default:
+                image = R.drawable.default_image;
+        }
+        return image;
+    }
+
+    private void saveWinsAndLosses(int wins, int losses) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("wins", wins);
+        editor.putInt("losses", losses);
+        editor.apply();
+    }
+
+    private void loadWinsAndLosses() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        wins = sharedPreferences.getInt("wins", 0);
+        losses = sharedPreferences.getInt("losses", 0);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        loadWinsAndLosses();
+
+        TextView lblWin = findViewById(R.id.lblWin);
+        lblWin.setText(String.valueOf(wins));
+
+        TextView lblLose = findViewById(R.id.lblLose);
+        lblLose.setText(String.valueOf(losses));
+
+    }
+
+
+
+}
